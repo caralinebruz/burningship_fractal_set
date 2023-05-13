a507bc2c6dfcfa0eb1303a17ff3103579d764ac7
caralinebruz <caraline.bruzinski@gmail.com>
2023-05-13 15:09:24 -0400

    computation is correct

--- simd/burningship_simd.cpp
+++ simd/burningship_simd.cpp
@@ -44,27 +44,21 @@ namespace
 
 
 
-
-
-
         for (int i = 0; i < rows; i++)
-        // for (int i = 1438; i < 1442; i++)
+        // for (int i = 4903; i < 4904; i++)
         {
             // get ci (ci will hold same values across the register because we go row by row)
             // ci = i / scaleY + y1
+            // printf("i:%d \n", i);
             double y0 = i / scaleY + y1;
 
             __m256d ci_vec = _mm256_set1_pd(y0);
 
 
-
             for (int j = 0; j < cols; j += 4)
-            // for (int j = 7604; j < 7612; j += 4)
+            //for (int j = 2900; j < 2910; j += 4)
             {
                 __m256d _j = _mm256_set_pd(j,j+1,j+2,j+3);
-                // double j_array[4];
-                // _mm256_storeu_pd(j_array, _j);
-                // printf("_j contents:  %f %f %f %f\n", j_array[3], j_array[2], j_array[1], j_array[0]);
 
                 // get cr (cr will hold diff values across the register)
                 // cr = j / scaleX + x1
@@ -77,16 +71,8 @@ namespace
                 __m256d re = _mm256_set1_pd(0.0);
                 __m256d im = _mm256_set1_pd(0.0);
 
-                //double my_array0[4];
-                //_mm256_storeu_pd(my_array0, cr_vec);
-                //printf("Vector contents: (row:%d col:%d) %f %f %f %f\n", i,j, my_array0[3], my_array0[2], my_array0[1], my_array0[0]);
-
                 // store the num iterations taken by each pixel
                 __m256d counter_vec = _mm256_set1_pd(0.0); // initially, each pixel has 0 iterations --- must be INTEGER, lets make it pd
-
-                // try this later, maybe enable me to exit early?
-                //__m256d max_vec = _mm256_set1_pd(max_iter); // epi32 for INTEGER, lets try it with pd
-
 
 
                 // Begin Algorithm
@@ -94,25 +80,14 @@ namespace
                 // start the main loop for this pixel
                 for (int t = 0; t < max_iter; t++) {
 
-                   // if (t > 2) {
-                   //     printf("t %d, row: %d col: %d \n", t, i, j);
-                    //}
-
                     // first generate the stopping criteria
                     // (zr * zr + zi * zi) > 4.0
                     __m256d zr2 = _mm256_mul_pd(zr, zr);
                     __m256d zi2 = _mm256_mul_pd(zi, zi);
                     __m256d sum = _mm256_add_pd(zr2, zi2);
 
-                    // print the SUM for debug
-                    //double my_array[4];
-                    //_mm256_storeu_pd(my_array, sum);
-                    //printf("SUM contents: (row:%d col:%d) %f %f %f %f\n", i,j, my_array[3], my_array[2], my_array[1], my_array[0]);
-                    // ok the sum is working
 
                     // create a mask to check each element in the vector for the condition
-                    // __m256d compare = _mm256_set1_pd(4.0);
-                    // __m256d mask = _mm256_cmp_pd(sum, compare, _CMP_LT_OQ); // do not modify the predicate it's working
                     __m256d mask = _mm256_cmp_pd(sum, _four, _CMP_LT_OQ);
                     // returns:
                     //          -nan when sum < 4
@@ -125,13 +100,6 @@ namespace
                     // -nan --> 1
                     mask = _mm256_and_pd(mask, _one);
 
-                    /*
-                    double my_array3[4];
-                    _mm256_storeu_pd(my_array3, mask);
-                    printf("MASK T1 contents: (row:%d col:%d) %f %f %f %f\n", i,j, my_array3[3], my_array3[2], my_array3[1], my_array3[0]);
-                    // ^ produces [1.0, 1.0, 0.0, 1.0] ==> 0.0 means {sum > 4} already. 1.0 means {sum < 1} 
-                    */
-
                     // each element in _cmp_result will be -nan (all bits set) 
                     //      if the corresponding element in mask is zero, 
                     // and zero (all bits clear) otherwise
@@ -141,33 +109,16 @@ namespace
                     // So, if all elements in mask are zero, mask2_int will be 15, 
                     // and if any element in mask is nonzero, the corresponding bit in mask2_int will be 0.
 
-                    if (mask2_int > 0) {
+                    if (mask2_int == 15) {
                         // so break when mask is 15 (all values in the original mask are 0000)
                         break;
                     }
-
-
-
-
-
-                    
-                    // int test_all_zeros = _mm256_testc_pd(_cmp_result, _mm256_set1_pd(-1.0));
-                    // if (test_all_zeros == 1) {
-                    // //     //printf("Stopping early at t=%d.\n", t);
-                    //      break;
-                    // }
-
 
                     // otherwise, add the mask to the counter vec
                     // pixels which have "escaped" by now should not accumulate more num_iterations
                     // pixels which have not "escaped" by now should +1 to the num of iterations needed to satisfy
                     counter_vec = _mm256_add_pd(counter_vec, mask);
                     
-                    // print the COUNTER AGG for debug
-                    // double my_array7[4];
-                    // _mm256_storeu_pd(my_array7, counter_vec);
-                    // printf("Counter contents: (row:%d col:%d) %f %f %f %f\n", i,j, my_array7[3], my_array7[2], my_array7[1], my_array7[0]);
-
                     // Actual Fractal Computation
                     // then, do the fractal computation for this single iteration
                     // z = abs(z * z) + c;
@@ -186,9 +137,9 @@ namespace
                 }
 
                 // print the COUNTER AGG for debug
-                //double my_array7[4];
-               // _mm256_storeu_pd(my_array7, counter_vec);
-               // printf("Counter contents: (row:%d col:%d) %f %f %f %f\n", i,j, my_array7[3], my_array7[2], my_array7[1], my_array7[0]);
+                // double my_array7[4];
+                // _mm256_storeu_pd(my_array7, counter_vec);
+                // printf("\t Final Counter contents: (row:%d col:%d) %f %f %f %f\n", i,_j, my_array7[3], my_array7[2], my_array7[1], my_array7[0]);
                 // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
                 // ********************* Grayscale value converter
@@ -203,30 +154,29 @@ namespace
                 // then i need to round this to the nearest integer
                 // https://www.intel.com/content/www/us/en/docs/intrinsics-guide/index.html#text=_mm256_round_pd&ig_expand=6159
                 __m256d grayscale_pd = _mm256_round_pd(values_mult, _MM_FROUND_TO_NEAREST_INT);
-
-
+                // double my_array73[4];
+                // _mm256_storeu_pd(my_array73, grayscale_pd);
+                // printf("\t   Rounded contents: (row:%d col:%d) %f %f %f %f\n", i,_j, my_array73[3], my_array73[2], my_array73[1], my_array73[0]);
 
                 
                 // ok now that I have my values, I can store them back in the pixel matrix
                 int index = i+j*rows;
 
-                // this part is suspect. there is def a better way to insert pd to ints
-                double grayscales_to_insert[4];
+                alignas(32) double grayscales_to_insert[4];
                 _mm256_store_pd(grayscales_to_insert, grayscale_pd);
 
-
-                // inverted, dont know why.
                 pixelMatrix[index + 0] = int(grayscales_to_insert[3]);
                 pixelMatrix[index + 1] = int(grayscales_to_insert[2]);
                 pixelMatrix[index + 2] = int(grayscales_to_insert[1]);
                 pixelMatrix[index + 3] = int(grayscales_to_insert[0]);
-                
 
-
+                // so, the computation is right, i have a grayscale value for every pixel, stored in the pixelmatrix.
+                // when writing the pixelmatrix to the image file, something is being skipped and i dont know what
+                // but the computation is correct.
             }
 
             if (i%200 == 0){
-                printf("row %d/%d \n", i, rows);
+                printf("\t\t row %d/%d \n", i, rows);
             }
         }
     }
@@ -239,7 +189,7 @@ namespace
             for (int j = 0; j < cols; j++)
             {
                 int grayscale_int = pixelMatrix[i+j*rows];
-                uchar value = (uchar) grayscale_int;
+                uchar value = (uchar) grayscale_int; 
                 img.ptr<uchar>(i)[j] = value;
             }
         }    
